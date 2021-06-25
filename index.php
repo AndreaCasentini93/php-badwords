b9605bc28cf46ff2b55a8fd4b9555f73333e33fb
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-25 15:06:19 +0200

    Refactoring

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-    /*
-    Creare una variabile con un paragrafo di testo a vostra scelta.
-    Stampare a schermo il paragrafo e la sua lunghezza.
-    Una parola da censurare viene passata dall'utente tramite parametro GET.
-    Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
-    */
-
-    $text = 'PHP è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche.';
-    $censored_text = str_replace($_GET["parola"], '***', $text);
+    $text = "PHP (acronimo ricorsivo di \"PHP: Hypertext Preprocessor\", preprocessore di ipertesti; originariamente acronimo di \"Personal Home Page\") è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. L'interprete PHP è un software libero distribuito sotto la PHP License. Attualmente è principalmente utilizzato per sviluppare applicazioni web lato server, ma può essere usato anche per scrivere script a riga di comando o applicazioni stand-alone con interfaccia grafica.";
+    $string = "censura";
+    $censored_text = str_replace($_GET[$string], '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -16,27 +10,27 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- FAVICON -->
+        <link rel="shortcut icon" href="img/php-logo.png" type="image/x-icon">
+        <!-- FONT GOOGLE -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <!-- STYLE CSS -->
+        <link rel="stylesheet" href="css/style.css">
         <!-- TITLE -->
         <title>PHP Badwords</title>
     </head>
 
     <body>
-        <h2>Paragrafo Originale</h2>
-        <h4>
-            <?php echo $text; ?>
-        </h4>
-        <p>
-            Il paragrafo è costituito da <?php echo strlen($text); ?> caratteri.
-        </p>
-
-        <br>
-
-        <h2>Paragrafo Censurato</h2>
-        <h4>
-            <?php echo $censored_text; ?>
-        </h4>
-        <p>
-            Il paragrafo è costituito da <?php echo strlen($censored_text); ?> caratteri.
-        </p>
+        <div class="wrapper">
+            <h1>Inserisci in Querystring la chiave "<span><?php echo $string; ?></span>" e come valore la parola che vuoi censurare</h1>
+            <h4>
+                <?php echo $censored_text; ?>
+            </h4>
+            <p>
+                Il paragrafo è costituito da <span><?php echo strlen($censored_text); ?></span> caratteri.
+            </p>
+        </div>
     </body>
 </html>
