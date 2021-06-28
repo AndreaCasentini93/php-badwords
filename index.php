930cb362226922d54bece367e2035581af0024af
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-28 10:29:28 +0200

    If Else

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
     $text = "PHP (acronimo ricorsivo di \"PHP: Hypertext Preprocessor\", preprocessore di ipertesti; originariamente acronimo di \"Personal Home Page\") è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. L'interprete PHP è un software libero distribuito sotto la PHP License. Attualmente è principalmente utilizzato per sviluppare applicazioni web lato server, ma può essere usato anche per scrivere script a riga di comando o applicazioni stand-alone con interfaccia grafica.";
     $string = "censura";
-    $censored_text = str_replace($_GET[$string], '***', $text);
+    if (!empty($_GET[$string])) {
+        $censored_text = str_replace($_GET[$string], '***', $text);
+    } else {
+        $censored_text = $text;
+    }
 ?>
 
 <!DOCTYPE html>
